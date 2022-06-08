013bb071ad44282ef5851b0345bc15cd25b55021
Kakerukitahara <takusan315@gmail.com>
2022-06-08 17:26:08 +0900

    [fix] Switch.jsの完成。

--- components/rpi/Switch.js
+++ components/rpi/Switch.js
@@ -1,51 +1,46 @@
 var channel;
 var switchPort;
+let penId;
 
-let connectServer = () => {
+tactMain();
+
+async function connectServer(arg) {
   var relay = RelayServer("achex", "chirimenSocket"); // 引数(無料サーバー, APIアドレス)
   channel = await relay.subscribe("chirimenSwitch");
+  penId = document.getElementById("penid");
+  console.log(penId.textContent);
 
-  let penId = document.getElementById("penid");
-  console.log(penId);
-};
-
-
-let setPort = (arg) => {
   var gpioAccess = await navigator.requestGPIOAccess();
-    let num;
-    if (arg === "StandSwitch") {
-      num = 999; // ペン置きスイッチなら999番ポートに接続する．
-    }
-    if (arg === "TactSwitch") {
-      num = 5; // タクトスイッチならば5番ポートを操作する．
-    }
+  let num = 6;
+  if (arg === "StandSwitch") {
+    num = 999; // ペン置きスイッチなら999番ポートに接続する．
+  }
+  if (arg === "TactSwitch") {
+    num = 6; // タクトスイッチならば5番ポートを操作する．
+  }
   switchPort = gpioAccess.ports.get(num);
   await switchPort.export("in");
-};
+  switchPort.onchange = action;
+}
 
-let action = (arg) => {
-
-  switchPort.onchange = function (val) {
-    // スイッチはPullupで離すと1なので反転させる．
-    val === 0 ? 1 : 0;
-  };
-
-  var sendData;
-  sendData.id = penId;
-  if (val === 0) { // スイッチOFF
-    sendData.state = 0;
-  }
-  else {
-    sendData.state = 1;
+function action(val) {
+  var sendData = {};
+  sendData.id = penId.textContentS;
+  if (val === 0) {
+    // スイッチON
+    sendData.state = true;
+  } else {
+    sendData.state = false;
   }
 
   sendData.address = "pc";
 
-  sendData.mode = arg;
+  sendData.mode = "TactSwitch";
 
   var jsonmsg = JSON.stringify(sendData);
+
   channel.send(jsonmsg);
-};
+}
 
 // async function standMain(){
 //   connectServer();
@@ -53,8 +48,6 @@ let action = (arg) => {
 //   action("StandSwitch");
 // };
 
-async function tactMain(){
-  connectServer();
-  setPort("TactSwitch");
-  action("TactSwitch");
-};
+async function tactMain() {
+  connectServer("TactSwitch");
+}
